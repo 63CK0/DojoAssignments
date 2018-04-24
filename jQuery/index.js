838291d01d9be9415bc2ce6884a51c6806e87775
Gecko <gecko@gecko.com>
2018-04-24 15:04:42 -0700

    learning jquery functions

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,24 +1,43 @@
 
+$(document).ready(function(){
+  $('#fadeOut').click(function(){
+    $('header h1').fadeOut('slow', function(){
 
-  $(document).ready(function(){
-    $('#fadeOut').click(function(){
-      $('header h1').fadeOut('slow', function(){
-
-      });
     });
+  });
 
-    $('#fadeIn').click(function(){
-      $('header h1').fadeIn('slow', function(){
+  $('#fadeIn').click(function(){
+    $('header h1').fadeIn('slow', function(){
 
-      });
     });
-    $('figure').click(function(){
-          $('figure').slideUp('slow', function() {
-            setTimeout(function() {
-              $('figure').slideDown('slow');
-            }, 1000);
-          });
+  });
+  $('#heisencat').click(function(){
+        $('#heisencat').slideUp(100, function() {
+          setTimeout(function() {
+            $('#heisencat').slideDown(100);
+          }, 1000);
         });
-
-
       });
+
+
+    $('#cat').after("<b>Google Links</b>");
+    $('#toggle').click(function()
+      {
+        $('#links').toggle(1000);
+      });
+
+
+
+
+    $('ul').hover(function() {
+      $(this).append("<li>Google ME</li>") ; }, function ()
+      {
+        $(this).find("li:last").remove();
+      }
+    );
+
+    $('input[type=checkbox]').change
+
+    )})
+
+    });
